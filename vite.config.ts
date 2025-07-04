a77bc7b2eeabbeea16a72cc28d89422fe5c45c55
nicolassere <nicosgtr@gmail.com>
2025-07-04 15:15:16 -0300

    delete unnecesary comment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Esto hace que use paths relativos
+  base: './', 
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
