d94762f21ee6d39f05ccffc8db78ae188dae4b77
nicolassere <nicosgtr@gmail.com>
2025-07-04 15:26:08 -0300

    delete test server

--- test-server/mi-app/BlochSphere/utils.js
+++
@@ -1,5 +0,0 @@
-//fetch a endpoints que pidan ya estar autenticado
-const fetchAutenticado = async (url, options = {method: "GET",credentials: "include", cache: "no-store"}) => {
-    return null
-  }
-
